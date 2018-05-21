8f96941922ae55056d84e96a98113ba2a46afeed
Matthew Orres <matthew.orres@gmail.com>
2018-05-21 11:18:09 -0400

    Use PropTypes, PureComponent

--- src/index.js
+++ src/index.js
@@ -1,71 +1,82 @@
-import { Component } from "react"
+import {PureComponent} from 'react';
+import PropTypes from 'prop-types';
 
 const eventsChanged = (yeoldevents, yonnewevents) =>
-  yeoldevents.sort().toString() !== yonnewevents.sort().toString()
+  yeoldevents.sort().toString() !== yonnewevents.sort().toString();
 
-export default class Idle extends Component {
-  static defaultProps = {
-    defaultIdle: false,
-    render: () => null,
-    onChange: () => {},
-    timeout: 1000,
-    events: ["mousemove", "mousedown", "keydown", "touchstart", "scroll"]
+export default class Idle extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {idle: this.props.defaultIdle};
+    this.timeout = null;
   }
-
-  state = {
-    idle: this.props.defaultIdle
-  }
-
-  timeout = null
 
   componentDidMount() {
-    this.attachEvents()
-    this.setTimeout()
-  }
-
-  componentWillUnmount() {
-    this.removeEvents()
+    this.attachEvents();
+    this.setTimeout();
   }
 
   componentDidUpdate(prevProps) {
     if (eventsChanged(prevProps.events, this.props.events)) {
-      this.removeEvents()
-      this.attachEvents()
+      this.removeEvents();
+      this.attachEvents();
     }
+  }
+
+  componentWillUnmount() {
+    this.removeEvents();
   }
 
   attachEvents() {
     this.props.events.forEach(event => {
-      window.addEventListener(event, this.handleEvent, true)
-    })
+      window.addEventListener(event, this.handleEvent, true);
+    });
   }
 
   removeEvents() {
     this.props.events.forEach(event => {
-      window.removeEventListener(event, this.handleEvent, true)
-    })
+      window.removeEventListener(event, this.handleEvent, true);
+    });
   }
 
   handleChange(idle) {
-    this.props.onChange({ idle })
-    this.setState({ idle })
+    this.props.onChange({idle});
+    this.setState({idle});
   }
 
-  handleEvent = () => {
+  handleEvent() {
     if (this.state.idle) {
-      this.handleChange(false)
+      this.handleChange(false);
     }
-    clearTimeout(this.timeout)
-    this.setTimeout()
+
+    clearTimeout(this.timeout);
+    this.setTimeout();
   }
 
   setTimeout() {
     this.timeout = setTimeout(() => {
-      this.handleChange(true)
-    }, this.props.timeout)
+      this.handleChange(true);
+    }, this.props.timeout);
   }
 
   render() {
-    return this.props.render(this.state)
+    return this.props.render(this.state);
   }
 }
+
+Idle.propTypes = {
+  defaultIdle: PropTypes.bool,
+  render: PropTypes.func,
+  onChange: PropTypes.func,
+  timeout: PropTypes.number,
+  events: PropTypes.array,
+};
+
+Idle.defaultProps = {
+  defaultIdle: false,
+  render: () => null,
+  onChange: () => {},
+  timeout: 1000,
+  events: ['mousemove', 'mousedown', 'keydown', 'touchstart', 'scroll'],
+};
